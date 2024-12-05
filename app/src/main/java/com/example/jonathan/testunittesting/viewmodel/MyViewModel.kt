2e81cf22ea3b8dbb21970314d7e0933e06bdaa38
Sealight Clock <chbebram@hotmail.com>
2024-12-04 16:14:35 -0800

    Use Mockito.inline() to mock static Log.d().

--- app/src/main/java/com/example/jonathan/testunittesting/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/example/jonathan/testunittesting/viewmodel/MyViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.jonathan.testunittesting.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.jonathan.testunittesting.model.MyRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+
+private const val TAG = "TUT: MyViewModel"
 
 class MyViewModel : ViewModel() {
     private val repository = MyRepository()
@@ -12,6 +15,8 @@ class MyViewModel : ViewModel() {
     val data: StateFlow<String> = _data
     
     fun getData() {
+        Log.d(TAG, "getData")
+
         _data.value = repository.fetchData()
     }
 }
--- app/src/test/java/com/example/jonathan/testunittesting/MyViewModelTest.kt
+++ app/src/test/java/com/example/jonathan/testunittesting/MyViewModelTest.kt
@@ -1,15 +1,42 @@
 package com.example.jonathan.testunittesting
 
+import android.util.Log
 import com.example.jonathan.testunittesting.viewmodel.MyViewModel
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mockStatic
 
 class MyViewModelTest {
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
     @Test
     fun test_getData() {
-        val viewModel = MyViewModel()
-        viewModel.getData()
+        // Mock the static Log class
+        mockStatic(android.util.Log::class.java).use { logMock ->
+            // Define the behavior for Log.d
+            Mockito.`when`(Log.d(Mockito.anyString(), Mockito.anyString())).thenReturn(0)
 
-        assertEquals("Data fetched from the repository", viewModel.data.value)
+            // TODO: Remove this test block:
+            // Call the method
+            val result = Log.d("TestTag", "TestMessage")
+            // Verify the method was called
+            logMock.verify { Log.d("TestTag", "TestMessage") }
+            // Assert the mocked result
+            assert(result == 0)
+
+            // Actual test:
+            val viewModel = MyViewModel()
+            viewModel.getData()
+            assertEquals("Data fetched from the repository", viewModel.data.value)
+        }
     }
 }
